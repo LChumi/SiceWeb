81f706402c7983906e9d7133659742ca30ade7d2
LChumi <luischumi.9@gmail.com>
2023-11-11 16:18:27 -0500

    feat: correxcion y creacion de consumos de soap al backend y  manejo de modals

--- src/app/modules/user/components/comprobantes/comprobantes.component.ts
+++ src/app/modules/user/components/comprobantes/comprobantes.component.ts
@@ -24,6 +24,7 @@ export class ComprobantesComponent implements OnInit{
   listaComprobantes:ComprobElecGrande[]=[];
   searchText: string = '';
   filterComrpobantes: ComprobElecGrande[]=[];
+  totalComprobantes:number=0;
   showModal:boolean=false;
   xml:any='';
 
@@ -46,6 +47,7 @@ export class ComprobantesComponent implements OnInit{
         this.listaComprobantes = listaComprobantes;
         this.filterComrpobantes=listaComprobantes;
         this.isBuscando=false
+        this.totalComprobantes=listaComprobantes.length
       },
       error => {
         console.error('No se pudo obtener la lista ',error)
@@ -66,6 +68,7 @@ export class ComprobantesComponent implements OnInit{
         });
         this.listaComprobantes = listaComprobantes
         this.isBuscando=false;
+        this.totalComprobantes=listaComprobantes.length
       },
       error => {
         console.error('No se pudo obtener la lista ',error)
@@ -78,7 +81,6 @@ export class ComprobantesComponent implements OnInit{
     this.comprobanteService.getXml(comprobante.cco_codigo,comprobante.xmlf_empresa).subscribe(
       (dato:any) =>{
         this.xml= dato;
-        console.log(dato)
       }
     )
   }
@@ -106,6 +108,32 @@ export class ComprobantesComponent implements OnInit{
     );
   }
 
+  verAutorizacion(comprobante:ComprobElecGrande){
+    this.soapService.verAutorizacion(comprobante.xmlf_clave).subscribe(
+      (autorizacion:string)=>{
+        console.log(autorizacion)
+      }
+    )
+  }
+
+  verificarComprobante(comprobante:ComprobElecGrande){
+    this.soapService.verificarComprobante(comprobante.xmlf_clave).subscribe(
+      (verificacion:string)=>{
+        console.log(verificacion)
+      }
+    )
+  }
+
+  renviarComprobante(comprobante:ComprobElecGrande){
+    console.log(comprobante.cli_mail,comprobante.xml_tipoComprobante)
+    this.soapService.enviarComrpobante(this.xml,comprobante.cli_mail,comprobante.xml_tipoComprobante).subscribe(
+      response => console.log(response),
+      error => console.error(error)
+    )
+    this.verificarComprobante(comprobante);
+    this.verAutorizacion(comprobante);
+  }
+
   //----------------------Metodos a nivel de aplicacion y funciones-----------------
   listarComprobantes():void{
     if (this.empresa !== undefined){
@@ -121,10 +149,25 @@ export class ComprobantesComponent implements OnInit{
     this.listaComprobantes = this.filterComrpobantes.filter((comprobante)=>
       comprobante.xmlf_comprobante.toLowerCase().includes(this.searchText.toLowerCase())
     );
+    this.totalComprobantes=this.listaComprobantes.length
+  }
+  searchComprobantesTipo(){
+    this.listaComprobantes = this.filterComrpobantes.filter((comprobante)=>
+      comprobante.xmlf_comprobante.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+    this.totalComprobantes=this.listaComprobantes.length
   }
 
   selectComprobante(comprobante:ComprobElecGrande){
     this.selectedComprobante=comprobante
+  }
+
+  guardarCambios(){
+    const modalBody=document.querySelector(".modal-body");
+    if (modalBody){
+      this.xml=modalBody.textContent;
+      console.log(this.xml);
+    }
   }
 
   verOpciones(){
@@ -134,6 +177,9 @@ export class ComprobantesComponent implements OnInit{
     this.showModal=false;
   }
 
+
+
+  //----------------------Iconos Fontawesome-----------------
   protected readonly faFileInvoice = faFileInvoice;
   protected readonly faSearch = faSearch;
   protected readonly faFileLines = faFileLines;
--- src/app/modules/user/services/soap.service.ts
+++ src/app/modules/user/services/soap.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {API_URL} from "../../../core/constants/constants";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 @Injectable({
@@ -21,6 +21,29 @@ export class SoapService {
   verRespuesta(clave:string):Observable<any>{
     const body=`clave=${clave}`
     return this.http.post<any>(this.baseurl+'verRespuesta',body,{headers:this.httpHeaders})
+  }
+
+  verAutorizacion(clave:string):Observable<any>{
+    const body=`clave=${clave}`
+    return this.http.post<any>(this.baseurl+'obtieneAutorizacion',body,{headers:this.httpHeaders})
+  }
+
+  verificarComprobante(clave:string):Observable<any>{
+    const body=`clave=${clave}`
+    return this.http.post<any>(this.baseurl+'verificarComprobante',body,{headers:this.httpHeaders})
+  }
+
+  enviarComrpobante(xml:string ,email:string, tipo:number):Observable<any>{
+
+    const params=new HttpParams()
+      .set('email',email)
+      .set('tipo',tipo.toString());
+
+    const httpOptions={
+      headers:new HttpHeaders({'Content-Type':'application/json'}),
+      params:params
+    }
+    return this.http.post<any>(this.baseurl+'recibirComprobante/',xml,httpOptions)
   }
 
 }
